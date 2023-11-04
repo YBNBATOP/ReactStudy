932519f139c32e264f34bdbd26dc36b01423e233
unknown <serafim.ciobanu@student.howest.be>
2023-11-04 21:32:47 +0100

    Upon select of a recipe we can now get the ID for further manipulation

--- foodapp/src/App.jsx
+++ foodapp/src/App.jsx
@@ -5,16 +5,21 @@ import Nav from "./components/Nav";
 import "../src/assets/css/App.css";
 import Container from "./components/Container";
 import InnerContainer from "./components/InnerContainer";
+import FoodDetail from "./components/FoodDetail";
 
 function App() {
   const [foodData, setFoodData] = useState([]);
+  const [foodId, setFoodId] = useState("");
   return (
     <div>
       <Nav />
       <Search foodData={foodData} setFoodData={setFoodData} />
       <Container>
         <InnerContainer>
-          <FoodList foodData={foodData} />
+          <FoodList setFoodId={setFoodId} foodData={foodData} />
+        </InnerContainer>
+        <InnerContainer>
+          <FoodDetail foodId={foodId} />
         </InnerContainer>
       </Container>
     </div>
