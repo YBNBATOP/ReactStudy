f7b237574dc1812ae12c9e4f4e556d592c88ab9c
unknown <serafim.ciobanu@student.howest.be>
2023-11-04 17:44:34 +0100

    Changed the storage of Todos to objects instead of strings

--- todoapp/src/Components/Form.jsx
+++ todoapp/src/Components/Form.jsx
@@ -1,21 +1,23 @@
 import { useState } from "react";
 import styles from "../assets/css/form.module.css";
 export default function Form({ todos, setTodos }) {
-  const [todo, setTodo] = useState("");
+  //const [todo, setTodo] = useState("");
+
+  const [todo, setTodo] = useState({ name: "", done: false });
 
   function handleSubmit(e) {
     e.preventDefault();
     setTodos([...todos, todo]);
-    setTodo("");
+    setTodo({ name: "", done: false });
   }
   return (
     <form className={styles.todoform} onSubmit={handleSubmit}>
       <div className={styles.inputContainer}>
         <input
           className={styles.moderninput}
-          onChange={(e) => setTodo(e.target.value)}
+          onChange={(e) => setTodo({ name: e.target.value, done: false })}
           type="text"
-          value={todo}
+          value={todo.name}
           placeholder="Enter todo item..."
         />
         <button className={styles.modernButton} type="submit">
